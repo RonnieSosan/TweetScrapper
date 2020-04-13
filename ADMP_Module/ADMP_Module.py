1e77804d2aecde970262e20bb6c050612d74b002
Sosan Babajide <sosanbabajide@live.com>
2020-04-13 03:59:13 +0100

    Staging

--- ADMP_Module/ADMP_Module.py
+++ ADMP_Module/ADMP_Module.py
@@ -2,34 +2,57 @@ import json
 import pandas as pd
 import TwitterQuery as tq
 from datetime import datetime
+from datetime import timedelta
+from dateutil.relativedelta import *
 
-# Load twitter credentials from json file
-twitterCredentials = {}
-with open("twitter_credentials.json", "r") as file:
-    twitterCredentials = json.load(file)
 
-year = 2020
+# set dates for query
+fromdate = '2017-01-01'
+todate = '2020-01-31'
+search_term = 'Knife Crime'
+
+
+#twitterCredentials = {}
+#with open("twitter_credentials.json", "r") as file:
+    #twitterCredentials = json.load(file)
+
+
+year = 2017
 formated_tweets = []
-index = '1100047155438473216'
 
-while year >= 2017 :
-    twitter_querier = tq.TwitterApiConstruct(twitterCredentials)
+# set the loop for running through the years until 2019
+while year <= 2019 :
+    twitter_querier = tq.TwitterApiConstruct()
+
+    #list_of_tweets = twitter_querier.get_user_timeLine_new('syptweet', fromdate, todate, 10)
     
     try:
-        list_of_tweets = twitter_querier.get_user_timeLine(index)
+        list_of_tweets = twitter_querier.query_tweets('20km', search_term, fromdate, todate, 'Sheffield, United Kingdom', 100)
 
         if list_of_tweets == []:
             break
-    
+        
+        #loop through each tweet
         for tweet in list_of_tweets:
-            datetime_object = datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S %z %Y')
-            dictionary_of_tweets = {"id": tweet["id"], "Date": str(datetime_object), "DisplayName": tweet["user"]["screen_name"], "Tweet": tweet["text"], "Location": tweet["user"]["location"]}
+            #create a time object from the tweet date
+            datetime_object = datetime.strptime(tweet.formatted_date, '%a %b %d %H:%M:%S %z %Y')
+
+            #create a dictionary for the tweet
+            dictionary_of_tweets = {"id": tweet.id, "Date": str(datetime_object), "username": tweet.username, "Tweet": tweet.text, "Location": tweet.geo}
+
+            #append the tweet to the list of formatted tweets
             formated_tweets.append(dictionary_of_tweets)
-            index = tweet["id"]
-            year = datetime_object.year
-    except :
-        break
+            index = tweet.id
+            date = datetime_object
+
+        fromdate = (datetime.strptime(fromdate, "%Y-%m-%d") + relativedelta(months =+ 1)).strftime("%Y-%m-%d")
+        todate = (datetime.strptime(todate, "%Y-%m-%d") + relativedelta(months =+ 1)).strftime("%Y-%m-%d")
+        year = datetime.strptime(todate, "%Y-%m-%d").year
+
+    except:
+        raise
+        #break
 
 df = pd.DataFrame(formated_tweets)
-df.to_csv(r'C:/Users/sosan/Documents/ADMP/ClassWorkData/crimeTweets.csv', index = False)
+df.to_csv(r'C:/Users/sosan/Documents/ADMP/ClassWorkData/{1}_result.csv'.format(search_term), index = False)
 
--- ADMP_Module/TwitterQuery.py
+++ ADMP_Module/TwitterQuery.py
@@ -1,13 +1,14 @@
 # Import the Twython class
 from twython import Twython
 import json
+import GetOldTweets3 as got
 
 class TwitterApiConstruct:
+
     def __init__(self, creds={}):
         self.credentials = creds
 
-    def get_user_timeLine(self, max_id=''):
-        
+    def get_user_timeLine(self, max_id=''):   
         api_response = []
         # Instantiate an object
         python_tweets = Twython(self.credentials['CONSUMER_KEY'], self.credentials['CONSUMER_SECRET'])
@@ -22,8 +23,37 @@ class TwitterApiConstruct:
 
         try:
             api_response = python_tweets.get_user_timeline(**query)
-            result = python_tweets.search(**{"q":"crime", "until": "2019-03-30"})
         except:
             return api_response
 
         return api_response
+    
+    def get_user_timeLine_new(self, username = '', from_date = '', to_date = '', number_of_tweets = 10):
+
+         # generate a query criteria 
+        tweetCriteria = got.manager.TweetCriteria().setUsername(username)\
+                                                .setSince(from_date)\
+                                                .setUntil(to_date)\
+                                                .setMaxTweets(number_of_tweets)\
+                                                .setEmoji("unicode")
+
+        #query for tweets from GetOldTweets3 using the query criteria
+        list_of_tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+        return list_of_tweets
+    
+    def query_tweets(self, range_in_km = '10km', search_term = '', from_date = '', to_date = '', location = '', number_of_tweets = 10):
+
+        tweetCriteria = got.manager.TweetCriteria().setQuerySearch(search_term).setNear(location).setWithin(range_in_km).setMaxTweets(number_of_tweets).setTopTweets(True)
+
+        if to_date != '':
+            tweetCriteria.setUntil(to_date)
+
+        if from_date != '':
+            tweetCriteria.setSince(from_date)
+
+        list_of_tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+        return list_of_tweets
+
+        
